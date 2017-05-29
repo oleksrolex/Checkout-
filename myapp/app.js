acce107f174c73e2880368c4241246923e9d2599
Samir Said <luckychild305@gmail.com>
2017-05-29 17:21:15 +0100

    updating files

--- myapp/app.js
+++ myapp/app.js
@@ -1,10 +1,33 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var nunjucks = require('nunjucks');
+
+
+nunjucks.configure(['views'], {
+    autoescape: true,
+    express: app
+});
+
+app.use('/public', express.static(__dirname + '/public'));
+app.set('view engine', 'html');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.render('new1')
+});
+
+app.post('/', function (req, res) {
+    console.log('you made post request!');
+    // connect to stripe
+    // ... from stripe documentation: https://stripe.com/docs/checkout/express
+
+
+    // IF success - thank you page
+    // res.render('thankyou');
+
+    // IF failure - display error
+    res.render('new1', {error: 'this is an error'})
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
